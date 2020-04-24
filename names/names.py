a7c27c8e5f5a71f8a2c74a2011ff962733c9ba11
John Afolabi <johnafolabi.da@gmail.com>
2020-04-24 09:43:59 +0100

    Complete names duplicate

--- names/names.py
+++ names/names.py
@@ -1,5 +1,5 @@
 import time
-
+from binary_search_tree import BinarySearchTree
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,14 +13,26 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# for i in range(len(names_1) - 1):
+#     if names_1[i] in names_2:
+#         duplicates.append(names_1[i])
+
+bst = BinarySearchTree(names_2[0])
+for i in range(1, len(names_2)):
+    bst.insert(names_2[i])
+
+for name in names_1:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
